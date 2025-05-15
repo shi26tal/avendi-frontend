977251acf65484b3128403091864ea1a652837e2
shi26tal <shital26prajapati@gmail.com>
2025-05-15 14:04:29 +0545

    done with about part

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -34,6 +34,62 @@ export const About = () => {
           </div>
         </div>
       </section>
+
+      {/* Why us? section */}
+      <section className="why-us-section">
+        <div className="why-container">
+          <div className="why-heading">
+            <h1>Why us? We add stars to your guest experience.</h1>
+            <img src="/5star.svg" alt="star" />
+          </div>
+          <div className="why-content">
+            <div className="row">
+              <div className="col3">
+                <div className="col-content">
+                  <div className="col-img">
+                    <img src="/flexible.svg" alt="flexible" />
+                  </div>
+                  <div className="col-text">
+                    <h2>Flexible</h2>
+                    <p>
+                      Our service is available in both a standalone model and
+                      with integration. Our standalone model can be deployed
+                      within 24 hours of our initial discussion/signup.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col3">
+                <div className="col-img">
+                  <img src="/analytics.svg" alt="flexible" />
+                </div>
+                <div className="col-text">
+                  <h2>Analytics</h2>
+                  <p>
+                    You can have easy access to insights into your guests'
+                    behavior inside and outside the hotel to improve the
+                    offerings and engagement.
+                  </p>
+                </div>
+              </div>
+              <div className="col3">
+                <div className="col-img">
+                  <img src="/support.svg" alt="flexible" />
+                </div>
+                <div className="col-text">
+                  <h2>Support</h2>
+                  <p>
+                    You will be assigned a dedicated support team that can cater
+                    to your needs. Our support staff can help you set up your
+                    admin panel, customize the menu and provide assistance when
+                    required.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
